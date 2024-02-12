467c15bbc7f0e52aef13676f2e87ac511db46527
Shuntaro Nakano <nshun583@gmail.com>
2024-02-13 00:47:34 +0900

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,14 @@
 terraform {
 
+  /*
   cloud {
-    organization = "organization-name"
+    organization = "nshun583_aws_test"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+  */
 
   required_providers {
     aws = {
